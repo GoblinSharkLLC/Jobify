e3717915e8e59057c72526c7a95b8680f86868fb
a-straus <straustech@gmail.com>
2020-05-09 12:37:21 -0400

    figured out styling of navbar

--- src/client/components/NavPanel.js
+++ src/client/components/NavPanel.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, NavItem, Nav, Col, Button } from 'react-bootstrap';
 
-export default function NavPanel() {
+export default function NavPanel({ userName }) {
   return (
     <div>
       <div>
         <Navbar bg="light">
-          <Navbar.Brand as={Link} to="/">
-            Jobly
-          </Navbar.Brand>
-          {/* <Link to="/">Find Jobs</Link> */}
-          {/* <Link to="/jobs">Saved Jobs</Link>
-        <Link to="/login">Login</Link> */}
+          <Col xs={1} />
+          <Col xs={2} />
+          <Col>
+            <Button variant="outline-secondary" as={Link} to="/">
+              Find Jobs
+            </Button>
+          </Col>
+          <Col>
+            <Navbar.Brand className="h1">Jobly</Navbar.Brand>
+          </Col>
+          <Button variant="outline-secondary" as={Link} to="/jobs">
+            Saved Jobs
+          </Button>
+          <Col xs={2} />
+          <Col xs={1}>
+            {userName ? (
+              <NavItem>{userName}</NavItem>
+            ) : (
+              <Button variant="outline-primary" as={Link} to="/login">
+                Log In
+              </Button>
+            )}
+          </Col>
         </Navbar>
       </div>
     </div>

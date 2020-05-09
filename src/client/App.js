bc4a948d3bb12eec1121be6ccac0755102ef0795
a-straus <straustech@gmail.com>
2020-05-09 11:28:08 -0400

    Pulled browserRouter out of app and into index.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavPanel from "./components/NavPanel";
-import MainPage from "./components/MainPage";
-import JobContainer from "./components/JobContainer";
-import Login from "./components/Login";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavPanel from './components/NavPanel';
+import MainPage from './components/MainPage';
+import JobContainer from './components/JobContainer';
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <NavPanel />
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/jobs" component={JobContainer} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <NavPanel />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/jobs" component={JobContainer} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </div>
   );
 }

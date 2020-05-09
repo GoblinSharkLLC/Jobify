f727031c7f9098ec355f4a89dea85f7d11c27d7a
a-straus <straustech@gmail.com>
2020-05-09 11:59:38 -0400

    got bootstrap styling to appear

--- src/client/components/NavPanel.js
+++ src/client/components/NavPanel.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 
 export default function NavPanel() {
   return (
-    <div className="nav-panel">
-      <Link to="/">Find Jobs</Link>
-      <Link to="/jobs">Saved Jobs</Link>
-      <Link to="/login">Login</Link>
+    <div>
+      <div>
+        <Navbar bg="light">
+          <Navbar.Brand as={Link} to="/">
+            Jobly
+          </Navbar.Brand>
+          {/* <Link to="/">Find Jobs</Link> */}
+          {/* <Link to="/jobs">Saved Jobs</Link>
+        <Link to="/login">Login</Link> */}
+        </Navbar>
+      </div>
     </div>
   );
 }

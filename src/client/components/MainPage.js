9dce2f202d9231ab35515f537002e80db2469549
Oluwajomiloju Ayobami Olaode <oaolaode@wpi.edu>
2020-05-11 12:31:53 -0400

    Formatted the Nav and Login with App so they talk to one another using useEffect hook

--- src/client/components/MainPage.js
+++ src/client/components/MainPage.js
@@ -1,52 +1,52 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Job from "./Job";
-import { Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Job from './Job';
+import { Button } from 'react-bootstrap';
 
 const placeHolderJobs = [
   {
     id: 1,
-    title: "Lead Front End Developer",
-    company: "FaceBook",
+    title: 'Lead Front End Developer',
+    company: 'FaceBook',
     image:
-      "https://imagoearth.org/wp-content/uploads/2019/12/facebook-logo.png",
-    url: "https://www.facebook.com/hire",
-    city: "New York",
-    state: "NY",
-    status: "Open",
-    posted: "May 5, 2020",
-    description: "Be an Engineer at facebook!",
-    contact: "Mr.",
-    notes: "Whats up",
+      'https://imagoearth.org/wp-content/uploads/2019/12/facebook-logo.png',
+    url: 'https://www.facebook.com/hire',
+    city: 'New York',
+    state: 'NY',
+    status: 'Open',
+    posted: 'May 5, 2020',
+    description: 'Be an Engineer at facebook!',
+    contact: 'Mr.',
+    notes: 'Whats up',
   },
   {
     id: 2,
-    title: "Mid-Level Software Engineer at Oracle",
-    company: "Oracle Inc.",
-    image: "",
-    url: "Https://www.Oracle.com/this/is/a/cool/job",
-    city: "Boston",
-    state: "MA",
-    status: "OPEN",
-    posted: "May 1, 2020",
+    title: 'Mid-Level Software Engineer at Oracle',
+    company: 'Oracle Inc.',
+    image: '',
+    url: 'Https://www.Oracle.com/this/is/a/cool/job',
+    city: 'Boston',
+    state: 'MA',
+    status: 'OPEN',
+    posted: 'May 1, 2020',
     description:
-      "Come be an engineer at oracle and let Larry Ellison kick you in the nuts every 3 hours",
-    contact: "Mr.",
-    notes: "",
+      'Come be an engineer at oracle and let Larry Ellison kick you in the nuts every 3 hours',
+    contact: 'Mr.',
+    notes: '',
   },
 ];
 
 export default function MainPage() {
   const [jobs, setJobs] = useState(placeHolderJobs);
-  const [title, setTitle] = useState("");
-  const [loc, setLoc] = useState("");
+  const [title, setTitle] = useState('');
+  const [loc, setLoc] = useState('');
 
   const handleSubmit = async (event) => {
     const saveInput = [title, loc];
-    setTitle("");
-    setLoc("");
+    setTitle('');
+    setLoc('');
     console.log(saveInput);
-    const result = await axios.get("/api/search", {
+    const result = await axios.get('/api/search', {
       params: {
         description: saveInput[0],
         location: saveInput[1],
@@ -60,7 +60,7 @@ export default function MainPage() {
   const handleChange = (event) => {
     event.preventDefault();
     event.persist();
-    if (event.target.name === "query1") {
+    if (event.target.name === 'query1') {
       setTitle(event.target.value);
     } else {
       setLoc(event.target.value);
